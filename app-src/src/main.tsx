917ceb6b26125974ab5b142d3e2234f9c653e143
kmaro16128793-create <kmaro16128793@gmail.com>
2026-09-10 07:37:32 +0200

    Activate audio Gold Master quote email workflow

--- app-src/src/main.tsx
+++ app-src/src/main.tsx
@@ -3,17 +3,21 @@ import ReactDOM from 'react-dom/client';
 import VideoExactApp from './VideoExactApp';
 import SafetyBoundary from './SafetyBoundary';
 import DeliveryExperience from './DeliveryExperience';
+import AudioGoldMasterExperience from './AudioGoldMasterExperience';
 import './styles.css';
 import './hardening.css';
 import './video-exact.css';
 import './delivery-experience.css';
+import './audio-gold-master.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <SafetyBoundary>
-      <DeliveryExperience>
-        <VideoExactApp />
-      </DeliveryExperience>
+      <AudioGoldMasterExperience>
+        <DeliveryExperience>
+          <VideoExactApp />
+        </DeliveryExperience>
+      </AudioGoldMasterExperience>
     </SafetyBoundary>
   </React.StrictMode>
 );
