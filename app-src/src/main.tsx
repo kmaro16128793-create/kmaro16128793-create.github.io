425cdd1f2b304cca43169bae5e5e100cd5b4d48b
kmaro16128793-create <kmaro16128793@gmail.com>
2026-09-10 07:43:07 +0200

    Enforce required customer email before quote flow

--- app-src/src/main.tsx
+++ app-src/src/main.tsx
@@ -4,6 +4,7 @@ import VideoExactApp from './VideoExactApp';
 import SafetyBoundary from './SafetyBoundary';
 import DeliveryExperience from './DeliveryExperience';
 import AudioGoldMasterExperience from './AudioGoldMasterExperience';
+import AudioRouteGuard from './AudioRouteGuard';
 import './styles.css';
 import './hardening.css';
 import './video-exact.css';
@@ -13,11 +14,13 @@ import './audio-gold-master.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <SafetyBoundary>
-      <AudioGoldMasterExperience>
-        <DeliveryExperience>
-          <VideoExactApp />
-        </DeliveryExperience>
-      </AudioGoldMasterExperience>
+      <AudioRouteGuard>
+        <AudioGoldMasterExperience>
+          <DeliveryExperience>
+            <VideoExactApp />
+          </DeliveryExperience>
+        </AudioGoldMasterExperience>
+      </AudioRouteGuard>
     </SafetyBoundary>
   </React.StrictMode>
 );
